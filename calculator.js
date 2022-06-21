c5da7cc93e99d36904135825e125dcea1d0133d0
showgologo <showgologo@localhost.localdomain>
2022-06-21 16:01:20 +0100

    A javascript only calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 // A javaScript Only Calculator by showgologo
 
-var firstNumber = window.prompt("Input Fist Number");
+var firstNumber = window.prompt("Input First Number");
 var secondNumber = window.prompt("Input Second Number");
-var operator = window.prompt("Choose Operator + - / * %");
+var operator = window.prompt("Choose Operator {+} {-} {/} {*} {%}");
 
 if (operator == '+') {
     alert(Number(firstNumber) + Number(secondNumber));
