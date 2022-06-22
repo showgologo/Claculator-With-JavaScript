9a3dea09fab9fe791d801c7abc327cbfeb9033fa
showgologo <showgologo@localhost.localdomain>
2022-06-22 10:34:28 +0100

    A javascript only calculator

--- calculator.js
+++ calculator.js
@@ -5,15 +5,15 @@ var secondNumber = window.prompt("Input Second Number");
 var operator = window.prompt("Choose Operator {+} {-} {/} {*} {%}");
 
 if (operator == '+') {
-    alert(Number(firstNumber) + Number(secondNumber));
+    alert('Your answer is ' + (Number(firstNumber) + Number(secondNumber)));
 } else if (operator == "-") {
-    alert(Number(firstNumber) - Number(secondNumber));
+    alert('Your answer is ' + Number(firstNumber) - Number(secondNumber));
 } else if (operator == "/") {
-    alert(Number(firstNumber) / Number(secondNumber));
+    alert('Your answer is ' + Number(firstNumber) / Number(secondNumber));
 } else if (operator == "*") {
-    alert(Number(firstNumber) * Number(secondNumber));
+    alert('Your answer is ' + Number(firstNumber) * Number(secondNumber));
 } else if (operator == "%") {
-    alert(Number(firstNumber) % Number(secondNumber));
+    alert('Your answer is ' + Number(firstNumber) % Number(secondNumber));
 } else {
     alert('Pick out of the available operator')
 }
